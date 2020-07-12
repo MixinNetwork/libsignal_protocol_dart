6cfe7475ff503c312c0f4d2851553502c8373cfa
Crossle Song <crosslesong@gmail.com>
2020-07-13 00:18:47 +0800

    Fix AES

--- lib/src/cbc.dart
+++ lib/src/cbc.dart
@@ -1,40 +1,23 @@
 import 'dart:typed_data';
 
-import 'package:pointycastle/api.dart';
-import 'package:pointycastle/block/aes_fast.dart';
-import 'package:pointycastle/block/modes/cbc.dart';
+import 'package:encrypt/encrypt.dart';
 
-Uint8List aesCbcEncrypt(
-    Uint8List key, Uint8List iv, Uint8List paddedPlaintext) {
-  final cbc = CBCBlockCipher(AESFastEngine())
-    ..init(true, ParametersWithIV(KeyParameter(key), iv));
+Uint8List aesCbcEncrypt(Uint8List key, Uint8List ivBytes, Uint8List plainText) {
+  final k = Key(key);
+  final iv = IV(ivBytes);
 
-  // Encrypt the plaintext block-by-block
-  final cipherText = Uint8List(paddedPlaintext.length); // allocate space
-
-  var offset = 0;
-  while (offset < paddedPlaintext.length) {
-    offset += cbc.processBlock(paddedPlaintext, offset, cipherText, offset);
-  }
-  assert(offset == paddedPlaintext.length);
-  return cipherText;
+  final encrypter = Encrypter(AES(k));
+  final encrypted = encrypter.encryptBytes(plainText, iv: iv);
+  return encrypted.bytes;
 }
 
-Uint8List aesCbcDecrypt(Uint8List key, Uint8List iv, Uint8List cipherText) {
-  // Create a CBC block cipher with AES, and initialize with key and IV
+Uint8List aesCbcDecrypt(
+    Uint8List key, Uint8List ivBytes, Uint8List cipherText) {
+  final k = Key(key);
+  final iv = IV(ivBytes);
 
-  final cbc = CBCBlockCipher(AESFastEngine())
-    ..init(false, ParametersWithIV(KeyParameter(key), iv)); // false=decrypt
-
-  // Decrypt the cipherText block-by-block
-
-  final paddedPlainText = Uint8List(cipherText.length); // allocate space
-
-  var offset = 0;
-  while (offset < cipherText.length) {
-    offset += cbc.processBlock(cipherText, offset, paddedPlainText, offset);
-  }
-  assert(offset == cipherText.length);
-
-  return paddedPlainText;
+  final encrypter = Encrypter(AES(k));
+  final decrypted = encrypter.decryptBytes(Encrypted(cipherText), iv: iv);
+  print(decrypted);
+  return decrypted;
 }
