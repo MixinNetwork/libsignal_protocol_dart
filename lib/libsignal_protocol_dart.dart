c45cd1364ebdeec3a47c2f5b6e08ea504d129e6a
Crossle Song <crosslesong@gmail.com>
2020-07-29 18:00:49 +0800

    Fix export

--- lib/libsignal_protocol_dart.dart
+++ lib/libsignal_protocol_dart.dart
@@ -1,48 +1,45 @@
 library libsignal_protocol_dart;
 
-import 'dart:convert';
-import 'dart:typed_data';
+export 'src/DecryptionCallback.dart';
+export 'src/SessionCipher.dart';
+export 'src/SessionBuilder.dart';
+export 'src/SignalProtocolAddress.dart';
+export 'src/IdentityKey.dart';
+export 'src/IdentityKeyPair.dart';
+export 'src/InvalidKeyException.dart';
+export 'src/InvalidKeyIdException.dart';
+export 'src/LegacyMessageException.dart';
+export 'src/DuplicateMessageException.dart';
+export 'src/NoSessionException.dart';
+export 'src/UntrustedIdentityException.dart';
 
-import 'src/DecryptionCallback.dart';
-import 'src/SessionCipher.dart';
-import 'src/SessionBuilder.dart';
-import 'src/SignalProtocolAddress.dart';
-import 'src/IdentityKey.dart';
-import 'src/IdentityKeyPair.dart';
-import 'src/InvalidKeyException.dart';
-import 'src/InvalidKeyIdException.dart';
-import 'src/LegacyMessageException.dart';
-import 'src/DuplicateMessageException.dart';
-import 'src/NoSessionException.dart';
-import 'src/UntrustedIdentityException.dart';
+export 'src/ecc/Curve.dart';
+export 'src/ecc/DjbECPrivateKey.dart';
+export 'src/ecc/DjbECPublicKey.dart';
+export 'src/ecc/ECKeyPair.dart';
+export 'src/ecc/ECPrivateKey.dart';
+export 'src/ecc/ECPublicKey.dart';
 
-import 'src/ecc/Curve.dart';
-import 'src/ecc/DjbECPrivateKey.dart';
-import 'src/ecc/DjbECPublicKey.dart';
-import 'src/ecc/ECKeyPair.dart';
-import 'src/ecc/ECPrivateKey.dart';
-import 'src/ecc/ECPublicKey.dart';
+export 'src/groups/GroupCipher.dart';
+export 'src/groups/GroupSessionBuilder.dart';
+export 'src/groups/SenderKeyName.dart';
 
-import 'src/groups/GroupCipher.dart';
-import 'src/groups/GroupSessionBuilder.dart';
-import 'src/groups/SenderKeyName.dart';
+export 'src/protocol/CiphertextMessage.dart';
+export 'src/protocol/PreKeySignalMessage.dart';
+export 'src/protocol/SenderKeyDistributuinMessage.dart';
+export 'src/protocol/SenderKeyMessage.dart';
+export 'src/protocol/SignalMessage.dart';
 
-import 'src/protocol/CiphertextMessage.dart';
-import 'src/protocol/PreKeySignalMessage.dart';
-import 'src/protocol/SenderKeyDistributuinMessage.dart';
-import 'src/protocol/SenderKeyMessage.dart';
-import 'src/protocol/SignalMessage.dart';
+export 'src/state/IdentityKeyStore.dart';
+export 'src/state/PreKeyBundle.dart';
+export 'src/state/PreKeyRecord.dart';
+export 'src/state/PreKeyStore.dart';
+export 'src/state/SessionRecord.dart';
+export 'src/state/SessionState.dart';
+export 'src/state/SessionStore.dart';
+export 'src/state/SignalProtocolStore.dart';
+export 'src/state/SignedPreKeyRecord.dart';
+export 'src/state/SignedPreKeyStore.dart';
 
-import 'src/state/IdentityKeyStore.dart';
-import 'src/state/PreKeyBundle.dart';
-import 'src/state/PreKeyRecord.dart';
-import 'src/state/PreKeyStore.dart';
-import 'src/state/SessionRecord.dart';
-import 'src/state/SessionState.dart';
-import 'src/state/SessionStore.dart';
-import 'src/state/SignalProtocolStore.dart';
-import 'src/state/SignedPreKeyRecord.dart';
-import 'src/state/SignedPreKeyStore.dart';
-
-import 'src/util/ByteUtil.dart';
-import 'src/util/KeyHelper.dart';
+export 'src/util/ByteUtil.dart';
+export 'src/util/KeyHelper.dart';
