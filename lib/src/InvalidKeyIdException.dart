acf6ed619f2714020175eab168bdcff0183558b0
Rhisiart ap Gwilym <rhisiart@hiafi.co.uk>
2020-10-11 14:48:36 +0100

    Add toString to Exceptions

--- lib/src/InvalidKeyIdException.dart
+++ lib/src/InvalidKeyIdException.dart
@@ -1,4 +1,7 @@
 class InvalidKeyIdException implements Exception {
   final String detailMessage;
   InvalidKeyIdException(this.detailMessage);
+
+  @override
+  String toString() => '$runtimeType - $detailMessage';
 }
