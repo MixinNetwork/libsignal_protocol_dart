cf40c73c0b52f6e4a20379d9cb19d3f0eea24399
Crossle Song <crosslesong@gmail.com>
2021-02-02 10:59:28 +0800

    Add encrypt message example

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 
@@ -14,11 +15,9 @@ void install() {
 
   var signedPreKey = KeyHelper.generateSignedPreKey(identityKeyPair, 0);
 
-  // ignore: unused_local_variable
   var sessionStore = InMemorySessionStore();
   var preKeyStore = InMemoryPreKeyStore();
   var signedPreKeyStore = InMemorySignedPreKeyStore();
-  // ignore: unused_local_variable
   var identityStore =
       InMemoryIdentityKeyStore(identityKeyPair, registerationId);
 
@@ -27,16 +26,34 @@ void install() {
   }
   signedPreKeyStore.storeSignedPreKey(signedPreKey.id, signedPreKey);
 
-  var remoteAddress = SignalProtocolAddress("remote", 1);
+  var remoteAddress = SignalProtocolAddress('remote', 1);
   var sessionBuilder = SessionBuilder(sessionStore, preKeyStore,
       signedPreKeyStore, identityStore, remoteAddress);
 
-  // sessionBuilder.processPreKeyBundle(retrievedPreKey);
+  // Should get remote from the server
+  var remoteRegId = KeyHelper.generateRegistrationId(false);
+  var remoteIdentityKeyPair = KeyHelper.generateIdentityKeyPair();
+  var remotePreKeys = KeyHelper.generatePreKeys(0, 110);
+  var remoteSignedPreKey =
+      KeyHelper.generateSignedPreKey(remoteIdentityKeyPair, 0);
+
+  var retrievedPreKey = PreKeyBundle(
+      remoteRegId,
+      1,
+      remotePreKeys[0].id,
+      remotePreKeys[0].getKeyPair().publicKey,
+      remoteSignedPreKey.id,
+      remoteSignedPreKey.getKeyPair().publicKey,
+      remoteSignedPreKey.signature,
+      remoteIdentityKeyPair.getPublicKey());
+
+  sessionBuilder.processPreKeyBundle(retrievedPreKey);
 
   var sessionCipher = SessionCipher(sessionStore, preKeyStore,
       signedPreKeyStore, identityStore, remoteAddress);
-  var ciphertext = sessionCipher.encrypt(utf8.encode("Hello Mixin"));
-
+  var ciphertext =
+      sessionCipher.encrypt(Uint8List.fromList(utf8.encode('Hello Mixin')));
+  print(ciphertext.serialize());
   //deliver(ciphertext);
 }
 
@@ -44,5 +61,5 @@ void groupSessioin() {
   var senderKeyName = SenderKeyName("", SignalProtocolAddress("sender", 1));
   var senderKeyStore = InMemorySenderKeyStore();
   var groupSession = GroupCipher(senderKeyStore, senderKeyName);
-  groupSession.encrypt(utf8.encode("Hello Mixin"));
+  groupSession.encrypt(Uint8List.fromList(utf8.encode('Hello Mixin')));
 }
