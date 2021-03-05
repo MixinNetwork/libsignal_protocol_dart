378119b552623ced84fc4d1bdf6af2fc98de8a11
Crossle Song <crosslesong@gmail.com>
2021-03-06 00:33:26 +0800

    Use pointycastle for aes

--- lib/src/cbc.dart
+++ lib/src/cbc.dart
@@ -1,12 +1,42 @@
 import 'dart:typed_data';
+
 import 'package:cryptography/cryptography.dart';
+import 'package:pointycastle/export.dart';
 
 Uint8List aesCbcEncrypt(Uint8List key, Uint8List iv, Uint8List plaintext) {
-  return aesCbc.encryptSync(plaintext,
-      secretKey: SecretKey(key), nonce: Nonce(iv));
+  final paddedPlaintext = pad(plaintext, 128);
+  final cbc = CBCBlockCipher(AESFastEngine())
+    ..init(true, ParametersWithIV(KeyParameter(key), iv)); // true=encrypt
+
+  final cipherText = Uint8List(paddedPlaintext.length); // allocate space
+  print(paddedPlaintext.length);
+  var offset = 0;
+  while (offset < paddedPlaintext.length) {
+    offset += cbc.processBlock(paddedPlaintext, offset, cipherText, offset);
+  }
+  assert(offset == paddedPlaintext.length);
+  return cipherText;
 }
 
-Uint8List aesCbcDecrypt(Uint8List key, Uint8List iv, Uint8List ciphertext) {
-  return aesCbc.decryptSync(ciphertext,
-      secretKey: SecretKey(key), nonce: Nonce(iv));
+Uint8List aesCbcDecrypt(Uint8List key, Uint8List iv, Uint8List cipherText) {
+  final cbc = CBCBlockCipher(AESFastEngine())
+    ..init(false, ParametersWithIV(KeyParameter(key), iv)); // false=decrypt
+
+  final paddedPlainText = Uint8List(cipherText.length); // allocate space
+  var offset = 0;
+  while (offset < cipherText.length) {
+    offset += cbc.processBlock(cipherText, offset, paddedPlainText, offset);
+  }
+  assert(offset == cipherText.length);
+  return unpad(paddedPlainText);
 }
+
+Uint8List pad(Uint8List bytes, int blockSize) {
+  final padLength = blockSize - (bytes.length % blockSize);
+  final padded = Uint8List(bytes.length + padLength)..setAll(0, bytes);
+  PKCS7Padding().addPadding(padded, bytes.length);
+  return padded;
+}
+
+Uint8List unpad(Uint8List padded) =>
+    padded.sublist(0, padded.length - PKCS7Padding().padCount(padded));
